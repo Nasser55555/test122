a7523a320cf3b82ea9ed39688be139060fdac407
zakaria <nabdo6461@gmail.com>
2025-07-10 15:21:38 +0100

    tesvvt

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
 import pandas as pd
-
-# Load the dataset
 df = pd.read_csv('/Users/zakaria/Desktop/ sas/DataSet.csv')
+df.head()
 
-# Display the shape (number of rows, number of columns)
-print("Dataset dimensions:", df.shape)
-# List all column names
-print("Available columns:")
-print(df.columns)
